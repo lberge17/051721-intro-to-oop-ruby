f9c55d7fb4779294d93a114140291d13fc138c2e
Laura Berge <lauraeberge@gmail.com>
2021-07-20 12:16:16 -0500

    post intro oop lecture

--- food.rb
+++ food.rb
@@ -1,6 +1,54 @@
 require "pry"
+require "securerandom"
+
+class Food
+  # attr_writer :type
+  # attr_reader :type
+  attr_accessor :type
+  @@all = []
+
+  # @type here == class instance variable
+  # puts @type
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  # def type=(arg)
+  #   @type = arg
+  # end
+
+  # def type
+  #   @type
+  # end
+
+  # def instance_method
+  #   @type # belongs to the instance
+  #   @@type # belongs to the class
+  # end
+
+  # def self.class_method
+  #   @type # belongs to the class
+  #   @@type # belongs to the class
+  # end
+end
 
 
+class Account
+  attr_reader :number
+  attr_writer :password
+
+  def initialize
+    @number = SecureRandom.hex(10)
+  end
+
+  def authenticate(password)
+    @password == password
+  end
+end
 
 
 binding.pry
